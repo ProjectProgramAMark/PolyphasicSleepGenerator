dd6d2565d2fe671b900082a487c1afe225a04867
ProjectProgramAMark <projectprogramamark@gmail.com>
2015-03-21 01:47:50 -0400

    Added nap3

--- src/Generator.java
+++ src/Generator.java
@@ -1,5 +1,4 @@
 import org.joda.time.Period;
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 /* Some notes about polyphasic sleep cycles:
@@ -24,12 +23,20 @@ public class Generator {
 
         Period coreTime = new Period(coreSleepHours, coreSleepMinutes, 0, 0);
         Period timeToFirstAndSecondNap = new Period(3, 42, 0, 0);
+        Period timeToThirdNap = new Period(6, 12, 0, 0);
         Period nap1 = new Period(coreTime.plus(timeToFirstAndSecondNap));
         Period nap2 = new Period(nap1.plus(timeToFirstAndSecondNap));
+        // Adding 18 minutes to naptime so nap1 time is included to nap2
         nap2 = nap2.plusMinutes(NAPTIME);
         if(nap2.getMinutes() > 60) {
             nap2 = nap2.minusMinutes(60);
             nap2 = nap2.plusHours(1);
+        }
+        Period nap3 = new Period(nap2.plus(timeToThirdNap));
+        nap3 = nap3.plusMinutes(NAPTIME);
+        if(nap3.getMinutes() > 60) {
+            nap3 = nap3.minusMinutes(60);
+            nap3 = nap3.plusHours(1);
         }
         boolean tryAgain = true;
 
@@ -81,18 +88,17 @@ public class Generator {
 
             // ------------ NAP 3 -------------- //
             if(whichFormat == 12) {
-                while (nap1.getHours() > 12) {
-                    nap1 = nap1.minusHours(12);
+                while (nap3.getHours() > 12) {
+                    nap3 = nap3.minusHours(12);
                 }
-                if (nap1.getMinutes() > 60) {
-                    nap1 = nap1.plusHours(1).minusMinutes(60);
-                    nap1 = (nap1.getHours() > 12) ? nap1.minusHours(12) : nap1;
+                if (nap3.getMinutes() > 60) {
+                    nap3 = nap3.plusHours(1).minusMinutes(60);
+                    nap3 = (nap3.getHours() > 12) ? nap3.minusHours(12) : nap3;
                 }
             } else if(whichFormat == 24) {
-                if(nap1.getMinutes() > 60) {
-                    nap1 = nap1.plusHours(1).minusMinutes(60);
+                if(nap3.getMinutes() > 60) {
+                    nap3 = nap3.plusHours(1).minusMinutes(60);
                 }
-
             } else {
                 System.out.println("You have entered an invalid number! Please try either 12 or 24.");
                 tryAgain = true;
@@ -103,7 +109,7 @@ public class Generator {
 
         return("\nYour sleep schedule is: \nCore Sleep: " + coreTime.getHours() + ":" + coreTime.getMinutes()
                 + "\nFirst Nap: " + nap1.getHours() + ":" + nap1.getMinutes() + "\nSecond Nap: " + nap2.getHours() +
-                ":" + nap2.getMinutes());
+                ":" + nap2.getMinutes() + "\nNap Three: " + nap3.getHours() + ":" + nap3.getMinutes());
     }
 
 }
